61d862600fdcae09b4c4102b88262da33b59a833
ummesadeka <sadekaahmed53@gmail.com>
2021-02-05 11:49:40 +0600

    true vs false

---
+++ true-vs-false.js
@@ -0,0 +1,18 @@
+//Falsy:
+// false
+// 0
+// ""
+// undefined
+// null
+// NaN
+
+//Truthy:
+//'0', ' ' []
+
+let name = 12;
+if(name  || name == 0){
+    console.log("condition is true");
+}
+else{
+    console.log("condition is false");
+}
