c8017b09fbae43fcb3cb08456ff9a85976a50b7c
ummesadeka <sadekaahmed53@gmail.com>
2021-02-05 18:56:08 +0600

    set time out

---
+++ settimeout.js
@@ -0,0 +1,11 @@
+const currentTime = new Date();
+//console.log(currentTime);
+console.log("first friend Happy birthday");
+setTimeout(function(){
+    console.log("second friend Happy birthday");
+}, 3000)
+setInterval(function(){
+    console.log("amake treat dey");
+}, 2000);
+console.log("third friend Happy birthday");
+console.log("fourth friend Happy birthday");
